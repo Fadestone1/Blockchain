3f4c062176eb58e4bf1830192b7d525ca9f9f5d1
Fadestone1 <41193297+Fadestone1@users.noreply.github.com>
2018-07-13 15:08:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
+const sha256 = require('cryptoJS-master/sha256');
+
 class Block {
-  constructor(index, timestamps, data, previoushash = ' '){
+  constructor(index, timestamp, data, previoushash = ' '){
     this.index = index;
-    this.timestamps = timestamps;
+    this.timestamps = timestamp;
     this.data = data;
     this.previoushash = previoushash;
-    this.hash = '';
+    this.hash = this.calculateHash();
     }
   calculateHash(){
-  
+      return sha256(this.index + this.previoushash + this.timestamp + JSON.stringify(this.data)).toString();
     }
   
 }
+
+class Blockchain{
+ constructor(){
+   this.chain = ();
+ }
+  
+  createGenesisBlock(){
+   return new Block(0, "01/01/2018", "Genesis block", "0"); 
+  }
+  
+  getLatestBlock(){
+    return this.chain(this.chain.lemgth - 1);
+  }
+  
+  addBlock(newBlock){
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let masseCoin = new Blockchain();
+masseCoin.addBlock(new Block(1, "12/07/2018", { amount: 4}));
+masseCoin.addBlock(new Block(2, "13/07/2018", { amount: 5}));
+
+
+
+
+
+
+
