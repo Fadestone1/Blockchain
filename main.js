4a69257bd583b9a2ece78579aa460efba5e19896
Fadestone1 <41193297+Fadestone1@users.noreply.github.com>
2018-07-13 14:20:02 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+class Block {
+  constructor(index, timestamp, data, previoushash = ' '){
+    this.index = index;
+    this.timestamps = timestamps;
+    this.data = data;
+    this.previoushash = previoushash;
+    this.hash = '';
+    }
+  calculateHash(){
+  
+    }
+  
+}
